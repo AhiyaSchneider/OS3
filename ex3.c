f013ef56126f64fe2fbedf3ae9ec99c2130b8f6e
Ahiya Schneider <achiaschnider@hotmail.com>
2023-06-03 22:26:32 +0300

    final

--- ex3.c
+++ ex3.c
@@ -4,6 +4,8 @@
 #include <pthread.h>
 #include <stdbool.h> 
 #include <unistd.h>
+#include <semaphore.h>
+#include <ctype.h>
 
 #define MAX_LINE_CONF 150
 #define CO_EDIT_QUEUES 3
@@ -30,6 +32,8 @@ typedef struct {
     int rear;  // initialize to -1 so indicate the queue is empty will point to last in queue
     char** articlessArr; 
     pthread_mutex_t mutex;  // Mutex for thread-safe access
+    sem_t empty;  // Semaphore to track empty slots
+    sem_t full;   // Semaphore to track filled slots 
 } BoundedQueue;
 
 typedef enum {
@@ -42,13 +46,14 @@ typedef enum {
 typedef struct UnBoundItem {
     char* article;
     UnBoundItem* next;
-    UnBoundItem* prev;
 } UnBoundItem;
 
 typedef struct {
     UnBoundItem* head;
     UnBoundItem* tail;
     pthread_mutex_t mutex;
+    sem_t empty;  // Semaphore to track empty slots
+    sem_t full;   // Semaphore to track filled slots
 } UnBoundQueue;
 
 typedef struct {
@@ -87,6 +92,8 @@ BoundedQueue* Bounded_Buffer(int size) {
     newQ->rear = 0;
     newQ->articlessArr = (char**)malloc(size * sizeof(char*));
     pthread_mutex_init(&(newQ->mutex), NULL);  // Initialize the mutex
+    sem_init(&(newQ->empty), 0, size);  // Initialize the empty semaphore with size as the initial value
+    sem_init(&(newQ->full), 0, 0);     // Initialize the full semaphore with 0 initial value
     return newQ;
 }
 
@@ -97,12 +104,9 @@ BoundedQueue* Bounded_Buffer(int size) {
  * @param queue: A pointer to the BoundedQueue object representing the bounded buffer.
  */
 void insert(char* s, BoundedQueue* queue) {
-    //check if it possible to insert a new
-    while ((queue->rear + 1) % queue->size == queue->front) {
-        // printf("stayee\n");
-        continue;
+    // Wait on the empty semaphore until there is an empty slot to insert
+    sem_wait(&(queue->empty));
 
-    }
     // Lock the mutex to ensure exclusive access to the queue
     pthread_mutex_lock(&(queue->mutex));
     
@@ -113,6 +117,9 @@ void insert(char* s, BoundedQueue* queue) {
 
     // Unlock the mutex to allow other threads to access the queue
     pthread_mutex_unlock(&(queue->mutex));
+
+    // Increment the full semaphore to indicate a filled slot is available
+    sem_post(&(queue->full));
 }
 
 /**
@@ -127,9 +134,13 @@ char* removeItem(BoundedQueue* queue) {
     char* item = NULL;
 
     // Check if the queue is empty
-    while (queue->front == queue->rear) {
-        continue;
-    }
+    // while (queue->front == queue->rear) {
+    //     continue;
+    // }
+
+    // Wait on the full semaphore until an item is available
+    sem_wait(&(queue->full));
+
     // Lock the mutex to ensure exclusive access to the queue
     pthread_mutex_lock(&(queue->mutex));
     // Retrieve the item from the queue
@@ -138,11 +149,11 @@ char* removeItem(BoundedQueue* queue) {
     // Increment the front index and wrap around if necessary
     queue->front = (queue->front + 1) % queue->size;
     
-
-    
-
     // Unlock the mutex to allow other threads to access the queue
     pthread_mutex_unlock(&(queue->mutex));
+
+    // Increment the empty semaphore to indicate an empty slot is available
+    sem_post(&(queue->empty));
 
     return item;
 }
@@ -189,6 +200,7 @@ void insertToUnBound(char* s, UnBoundQueue* queue) {
  *          if the unbounded queue is empty.
  */
 char* removeItemFromUnBound(UnBoundQueue* queue) {
+
     pthread_mutex_lock(&(queue->mutex));
 
     if (queue->head == NULL) {
@@ -211,7 +223,6 @@ char* removeItemFromUnBound(UnBoundQueue* queue) {
     return item;
 }
 
-
 //the code:
 /**
  * numOfProducers - Determines the number of threads based on the configuration file.
@@ -228,7 +239,6 @@ int numOfProducers(FILE* configFile) {
     while (fgets(buffer, sizeof(buffer), configFile) != NULL) {
         // Ignore empty lines
         if (strlen(buffer) > 1) {
-            // printf("1111the char:    %c, num:  %d  count:   %d\n", buffer[0], buffer[0], count);
             if( buffer[0] == '\n' || buffer[0] == ' ') {
                 if (count == 1) {
                     break;
@@ -247,7 +257,6 @@ int numOfProducers(FILE* configFile) {
             }
         }
     }
-    // printf("num of threads:    %d\n", numThreads);
     return numThreads;
 }
 
@@ -337,8 +346,6 @@ void* producer(void* arg) {
     int threadNum = prodArgs->threadNum;
     int numOfArticles = prodArgs->numOfArticles;
     BoundedQueue* queue = prodArgs->queue;
-    
-    // printf("thread number:   %d,   number of articles:    %d\n",threadNum, numOfArticles);
     int i = 0, randNum, commomCounter =0;
     int sportsCounter = 0, newsCounter = 0, weatherCounter = 0;
     for (i = 0; i < numOfArticles; i++)
@@ -365,7 +372,6 @@ void* producer(void* arg) {
                 type = "UNKNOWN";
                 break;
         }
-        // printf("the type is: %s  random number is:   %d\n", type, randNum);
         char message[100];
         sprintf(message, "Producer %d %s %d", threadNum, type, commomCounter);
         //allocate memory on heap so it remain
@@ -373,22 +379,22 @@ void* producer(void* arg) {
         strcpy(messagePtr, message);
         //insert message to queue
         insert(messagePtr, queue);
-        printf("in1  Generated article: %s\n", messagePtr);
+        //printf("in1  Generated article: %s\n", messagePtr);
     }
     char* finishMessage = malloc(strlen("Done") + 1);  // +1 for the null terminator
     strcpy(finishMessage, "Done");
     insert(finishMessage, queue);
-    printf("DONE INSERTED\n");
+    //printf("DONE INSERTED\n");
     free(prodArgs);
     pthread_exit(NULL);
 }
 
 
-void producerGenerate(int threadsNum, FILE* configFile, BoundedQueue** boundQueuesArr){
+void producerGenerate(pthread_t *threads, int threadsNum, FILE* configFile, BoundedQueue** boundQueuesArr){
     //make the N producers threads and feed the queues
     //create the dispatcher
     int i = 0;
-    pthread_t threads[threadsNum];
+    // pthread_t threads[threadsNum];
     //make the threads and send them and a quqeue to func
     rewind(configFile);
     for (i = 0; i < threadsNum; i++)
@@ -397,7 +403,7 @@ void producerGenerate(int threadsNum, FILE* configFile, BoundedQueue** boundQueu
         //put in config[0] the num Of Articles and in config[1] the size of bounded
         if(getInfoConfig(configFile, config)) {
             printf("failed to put data in config\n");
-            // return 1;
+            return;
             //deal with failure here
         }
         boundQueuesArr[i] = Bounded_Buffer(config[1]);
@@ -409,7 +415,6 @@ void producerGenerate(int threadsNum, FILE* configFile, BoundedQueue** boundQueu
         pthread_create(&threads[i], NULL, producer, producerArgs);
     }
 }
-
 
 bool boundQueueRead(bool* isDone, int i, BoundedQueue* boundedQueue, UnBoundQueue** unBoundQueues) {
     //check if this queue finished
@@ -430,7 +435,7 @@ bool boundQueueRead(bool* isDone, int i, BoundedQueue* boundedQueue, UnBoundQueu
             return true;
         } else {
             // Check the type of the message
-            printf("out2 from the queue :   %s\n", item);
+            //printf("out2 from the queue :   %s\n", item);
             if (strstr(item, "SPORTS") != NULL) {
                 // Process sports message
                 insertToUnBound(item, unBoundQueues[SPORTS_QUEUE]);
@@ -442,13 +447,12 @@ bool boundQueueRead(bool* isDone, int i, BoundedQueue* boundedQueue, UnBoundQueu
                 insertToUnBound(item, unBoundQueues[NEWS_QUEUE]);
             } else {
                 // Unknown type
-                printf("SOMETHING FAILED\n");
+                //printf("SOMETHING FAILED\n");
             }
         }
     }
     return false;
 }
-
 
 /**
  * Dispatcher function that processes items from multiple queues until all queues are done.
@@ -464,14 +468,12 @@ void* dispatcher(void* arg){
 
     bool* isDone = calloc(numQueues, sizeof(bool));  // Allocate the boolean array
     bool finish = false;
-
-    // printf("num of queues:   %d\n", numQueues);
     // Check if the allocation was successful
     if (isDone == NULL) {
         // Handle the error
         fprintf(stderr, "Failed to allocate memory for the boolean array.\n");
-    }
-    while (!finish) {
+    }else {
+        while (!finish) {
         finish = true;
         // printf("looping\n");
         for (int i = 0; i < numQueues; i++) {
@@ -497,28 +499,31 @@ void* dispatcher(void* arg){
             insertToUnBound(item1, coEditQueues[SPORTS_QUEUE]);
             insertToUnBound(item2, coEditQueues[WEATHER_QUEUE]);
             insertToUnBound(item3, coEditQueues[NEWS_QUEUE]);
-            printf("out2   DONE\n");
+            //printf("out2   DONE\n");
     //when finish send 3 queues "DONE"
     free(dispArgs);
     free(isDone);
+    }
+    
     pthread_exit(NULL);
 }
 
 
-void dispatcherGenerate(int threadsNum, BoundedQueue** boundQueuesArr, UnBoundQueue** unBoundQueuesArr) {
+void dispatcherGenerate(pthread_t* dispatcherThread, int threadsNum, BoundedQueue** boundQueuesArr, UnBoundQueue** unBoundQueuesArr) {
+    
     int i;
     for (i = 0; i < CO_EDIT_QUEUES; i++)
     {
         unBoundQueuesArr[i] = unBoundQueue();
     }
+    int sizeDispAloc = sizeof(DispatcherArguments);
 
-    DispatcherArguments* dispatcerArgs = malloc(sizeof(DispatcherArguments));
+    DispatcherArguments* dispatcerArgs = malloc(sizeDispAloc);
     dispatcerArgs->coEditQueues = unBoundQueuesArr;
     dispatcerArgs->numQueues = threadsNum;
     dispatcerArgs->queues = boundQueuesArr;
 
-    pthread_t dispatcherThread;
-    pthread_create(&dispatcherThread, NULL, dispatcher, dispatcerArgs);
+    pthread_create(&dispatcherThread[0], NULL, dispatcher, dispatcerArgs);
 }
 
 
@@ -531,7 +536,7 @@ void* coEditor(void* arg){
         char* item = removeItemFromUnBound(unBoundQueue);
         if (item != NULL) {
             if (strcmp(item, "Done") == 0){
-                printf("3 out     DONE\n");
+                //printf("3 out     DONE\n");
                 insert(item, screenQueue);
                 done = true;
                 break;
@@ -546,11 +551,8 @@ void* coEditor(void* arg){
 }
 
 
-void coEditorsGenerate(FILE* configFile, UnBoundQueue** unBoundQueuesArr, BoundedQueue* screenQueue) {
-    
-
+void coEditorsGenerate(pthread_t* threads, FILE* configFile, UnBoundQueue** unBoundQueuesArr, BoundedQueue* screenQueue) {
     // Transfer items from unBoundQueuesArr to screenQueue using multiple threads
-    pthread_t threads[3];
     for (int i = 0; i < 3; i++) {
         CoEditorArguments* coEditArgs = malloc(sizeof(CoEditorArguments));
         coEditArgs->screenQueue = screenQueue;
@@ -591,7 +593,7 @@ void* screenManager(void* arg) {
         }
         
         // Print the item to the screen
-        printf("6out   screenQueue: %s\n", item);
+        printf("%s\n", item);
 
         // Free the memory allocated for the item
         free(item);
@@ -619,8 +621,38 @@ void freeQueues(BoundedQueue** qTBQS, int threads, UnBoundQueue** qTUBQS, Bounde
 }
 
 
+int getSizeFromFile(FILE* configFile) {
+    int size = 0;
+    int currentSize = 0;
+    int digit;
+    int foundNumber = 0;
+
+    fseek(configFile, 0, SEEK_SET);  // Move the file pointer to the beginning of the file
+
+    // Read the file character by character
+    while ((digit = fgetc(configFile)) != EOF) {
+        if (isdigit(digit)) {
+            currentSize = (currentSize * 10) + (digit - '0');
+            foundNumber = 1;
+        } else {
+            if (foundNumber) {
+                size = currentSize;
+                foundNumber = 0;
+                currentSize = 0;
+            }
+        }
+    }
+
+    if (foundNumber) {
+        // If the file doesn't end with a non-digit character, consider the last sequence of digits as the size
+        size = currentSize;
+    }
+
+    return size;
+}
+
 int main(int argc, char* argv[]) {
-    printf("started\n");
+    //printf("started\n");
     //check if there are inough param and if the file can be opened.
     FILE* configFile;
     if (openFile(&configFile, argc, argv)) {
@@ -632,24 +664,26 @@ int main(int argc, char* argv[]) {
     // printf("number of threads is:  %d\n", threadsNum);
 
     BoundedQueue** boundQueuesArr = malloc(threadsNum * sizeof(BoundedQueue*));
-    producerGenerate(threadsNum, configFile,boundQueuesArr);
+    pthread_t threadsProducer[threadsNum];
+    producerGenerate(threadsProducer, threadsNum, configFile,boundQueuesArr);
 
     //implement dispatcher and three unbound queue
     UnBoundQueue** unBoundQueuesArr = malloc(CO_EDIT_QUEUES * sizeof(UnBoundQueue*));
-    dispatcherGenerate(threadsNum, boundQueuesArr, unBoundQueuesArr);
+    pthread_t dispatcherThread[1];
+    dispatcherGenerate(dispatcherThread, threadsNum, boundQueuesArr, unBoundQueuesArr);
 
     //when read 3 failed - read only one number so generate for each N,W,S editors a bounded queue
     //imp co-editors:
     BoundedQueue* screenQueue;
     // Read the size from the last line of the configFile
-    int size;
-    fseek(configFile, -sizeof(int), SEEK_END);
-    fread(&size, sizeof(int), 1, configFile);
-    fclose(configFile);
+    
+    int size = getSizeFromFile(configFile);
+    printf("size:  %d \n", size);
 
     // Create the bounded buffer with the specified size
     screenQueue = Bounded_Buffer(size);
-    coEditorsGenerate(configFile, unBoundQueuesArr, screenQueue);
+    pthread_t threadsCoEditor[3];
+    coEditorsGenerate(threadsCoEditor , configFile, unBoundQueuesArr, screenQueue);
 
     //implement screen maneger
     ScreenManagerArguments* screenArgs = malloc(sizeof(ScreenManagerArguments));
@@ -657,7 +691,19 @@ int main(int argc, char* argv[]) {
     pthread_t screenManagerThread;
     pthread_create(&screenManagerThread, NULL, screenManager, (void*)screenArgs);
 
+    
+    
     pthread_join(screenManagerThread, NULL);
+    pthread_join(dispatcherThread[0], NULL);
+    int i;
+    for (i = 0; i < threadsNum; i++)
+    {
+        pthread_join(threadsProducer[i], NULL);
+    }
+    for ( i = 0; i < 3; i++)
+    {
+        pthread_join(threadsCoEditor[i], NULL);
+    }
     freeQueues(boundQueuesArr, threadsNum, unBoundQueuesArr, screenQueue);
     return 0;
 }
